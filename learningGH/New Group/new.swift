b6b4b9be29ab39e9980d51635b5f37a2f8ebdf37
Ankita-Rawat <go2ankitarawat@gmail.com>
2021-12-07 07:32:34 +0530

    add new folder add new swift file

---
+++ learningGH/New Group/new.swift
@@ -0,0 +1,20 @@
+//
+//  new.swift
+//  learningGH
+//
+//  Created by Bharat Singh on 07/12/21.
+//
+
+import SwiftUI
+
+struct new: View {
+    var body: some View {
+        Text("a new view")
+    }
+}
+
+struct new_Previews: PreviewProvider {
+    static var previews: some View {
+        new()
+    }
+}
